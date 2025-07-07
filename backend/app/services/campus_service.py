377e4db81891401cb41e31b049e2ecdb6d19d5ac
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-07-07 22:02:45 +0300

    Implement get, create, update, and delete functions for campuses

--- backend/app/services/campus_service.py
+++ backend/app/services/campus_service.py
@@ -2,35 +2,36 @@ from app import db
 from app.models import Campus
 
 
-def get_all_campuses():
-    return Campus.query.all()
+class CampusService:
+    @staticmethod
+    def get_all_campuses():
+        return Campus.query.all()
 
+    @staticmethod
+    def get_campus_by_id(campus_id):
+        return Campus.query.get(campus_id)
 
-def get_campus_by_id(campus_id):
-    return Campus.query.get(campus_id)
-
-
-def create_campus(name, location, description=None):
-    new_campus = Campus(name=name, location=location, description=description)
-    db.session.add(new_campus)
-    db.session.commit()
-    return new_campus
-
-
-def update_campus(campus_id, name, location, description):
-    campus = Campus.query.get(campus_id)
-    if not campus:
-        return None
-
-    campus.name = name
-    campus.location = location
-    campus.description = description
-    db.session.commit()
-    return campus
-
-
-def delete_campus(campus_id):
-    campus = Campus.query.get(campus_id)
-    if campus:
-        db.session.delete(campus)
+    @staticmethod
+    def create_campus(name, location, description=None):
+        new_campus = Campus(name=name, location=location, description=description)
+        db.session.add(new_campus)
         db.session.commit()
+        return new_campus
+
+    @staticmethod
+    def update_campus(campus_id, name, location, description):
+        campus = Campus.query.get(campus_id)
+        if not campus:
+            return None
+        campus.name = name
+        campus.location = location
+        campus.description = description
+        db.session.commit()
+        return campus
+
+    @staticmethod
+    def delete_campus(campus_id):
+        campus = Campus.query.get(campus_id)
+        if campus:
+            db.session.delete(campus)
+            db.session.commit()
