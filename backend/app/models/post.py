ffe11b11447cf05324e1bc734fa3fe5d17035dfc
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-07-10 13:12:58 +0300

    post

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -16,6 +16,7 @@ class Post(db.Model):
     # Relationships
     author = db.relationship("User", back_populates="posts")
     campus = db.relationship("Campus", back_populates="posts")
+    user = db.relationship("User", back_populates="posts")
     group = db.relationship("Group", back_populates="posts")
     comments = db.relationship(
         "Comment", back_populates="post", cascade="all, delete-orphan", lazy="dynamic"
