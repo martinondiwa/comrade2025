3cf1291fb671f6e63302638fcae52bf0927b2a20
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-07-11 14:28:15 +0300

    Flask extensions

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
-# Initialize Flask extensions (no app context yet)
+#  Flask extensions 
 db = SQLAlchemy()
 jwt = JWTManager()
 migrate = Migrate()
